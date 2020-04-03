62e6c7f0c9f38abd2cfa1dc2fbb33811f9adcdae
Clément <poibeau.clement@gmail.com>
2020-04-03 14:03:22 +0200

    parser v2, final

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ path = 'D:/Documents/Winamax Poker/accounts/ArcLight__4/history'
 
 def listdirectory(path):
     files = glob.glob(path+'/*')
-    variante, position, nbjoueur, vitesse, mode, prizepool, buyin, gain = [], [], [], [], [], [], [], []
+    games = []
 
     for i in files:
 
@@ -16,37 +16,35 @@ def listdirectory(path):
                 a = open(i, 'r', encoding='utf-8')
                 raw = a.read()
 
-
                 if i[len(path)+1:].find('holdem_no')>-1 :
-                    variante.append('NLHE')
-
+                    variante = 'NLHE'
                 elif i[len(path)+1:].find('omaha_pot')>-1 :
-                    variante.append('PLO')
-
+                    variante = 'PLO'
                 else :
-                    variante.append('Other')
+                    variante = 'Other'
 
-                position.append(int(re.search('You finished in (\d+)', raw).group(1)))
+                position = int(re.search('You finished in (\d+)', raw).group(1))
 
-                nbjoueur.append(int(re.search('Registered players : (\d+)', raw).group(1)))
+                nbjoueur = int(re.search('Registered players : (\d+)', raw).group(1))
 
-                vitesse.append(re.search('Speed : (\S+)', raw).group(1))
+                vitesse = re.search('Speed : (\S+)', raw).group(1)
 
-                mode.append(re.search('Mode : (\S+)', raw).group(1))
+                mode = re.search('Mode : (\S+)', raw).group(1)
 
-                prizepool.append(re.search('Prizepool : (\d+\.?(\d+)?)€', raw).group(1))
+                prizepool = float(re.search('Prizepool : (\d+\.?\d*)€', raw).group(1))
                 
                 buyinraw = re.search('Buy-In : (\d+\.?\d*)€ \+ (\d+\.?\d*)€', raw)
-                buyin.append((buyinraw.group(1), buyinraw.group(2)))
-                
+                buyin = (float(buyinraw.group(1)), float(buyinraw.group(2)))
+
                 if re.search('You won (\d+\.?\d*)€', raw):
-                    gain.append(float(re.search('You won (\d+\.?\d*)€', raw).group(1)))
+                    gain = (float(re.search('You won (\d+\.?\d*)€', raw).group(1)))
                 else:
-                    gain.append(0)
+                    gain = (0)
 
                 a.close()
-        
-    return [variante, position, nbjoueur, vitesse, mode, prizepool, gain, buyin]
+                games.append((variante, position, nbjoueur, vitesse, mode, prizepool, gain, buyin))
+
+    return games
 
 
 result = listdirectory(path)
